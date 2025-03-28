d0055516687d5f11a30f1cb64c075335aef39ee0
“✨Anakraken✨” <anakrakennn@gmail.com>
2025-03-28 15:51:07 -0600

    update to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Modal } from './components/Modals';
 import { Button } from './components/Buttons';
-import './App.css';
 
 function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
