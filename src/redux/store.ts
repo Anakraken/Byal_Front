ad096cf10d82f6eeb0a9746bf8cabdc691064b84
“✨Anakraken✨” <anakrakennn@gmail.com>
2025-04-22 10:31:22 -0600

    refactor localstorage using redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,43 @@
 // configureStore permite dividir el estado en multiples archivos
-import { configureStore } from '@reduxjs/toolkit';
+// redux/store.ts
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // usa localStorage
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
 });
 
-// tipo del estado global
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'], // solo persistimos auth
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+//Agregan el type al estado global
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
